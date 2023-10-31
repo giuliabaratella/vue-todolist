482baf621fcf0c063c5d0bc153c68bf7cecedc3d
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-31 15:53:39 +0100

    added style (bgimage + h1 font) /added list / added line-through condition

--- js/script.js
+++ js/script.js
@@ -4,10 +4,35 @@ const { createApp } = Vue
 createApp({
     data(){
         return {
-
+            tasks: [
+                {
+                    text: 'Buy Halloween costume',
+                    done:false,
+                    id: 1
+                },
+                {
+                    text: 'Choose horror film to watch',
+                    done:true,
+                    id: 2
+                },
+                {
+                    text: 'Bake pumpkin cookies',
+                    done:false,
+                    id: 3
+                },
+                {
+                    text: 'Decorate the house',
+                    done:true,
+                    id: 4
+                },
+                
+            ],
         }
     },
     methods: {
 
     }
 }).mount('#app')
+
+// Stampare all'interno di una lista HTML un item per ogni todo.
+// Se la proprietà `done` è uguale a `true`, visualizzare il testo del todo ~~sbarrato~~.
